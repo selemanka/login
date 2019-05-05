94bdf70454955081436d32a461455977bc45bc4a
seleman.a <31805806+selemanka@users.noreply.github.com>
2019-05-05 16:28:14 +0300

    Add files via upload

---
+++ UserLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace seleman_coursework.Models
+{
+    public class UserLogin
+    {
+        [Display(Name="User ID")]
+        [Required(AllowEmptyStrings =false,ErrorMessage ="Email ID is required")]
+        public string EmailID { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
